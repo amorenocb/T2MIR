86bcb044cdf379b2e814ecdb50e4c19376f477d9
Juan Andrés <amorenocb@gmail.com>
2017-10-20 19:04:01 -0300

    Minor fixes to get_data_descriptors and get_data_dimensions functions. Plus modified the Dataset class so now it only takes one input file. It seemed cleaner to keep data sets Q and R separated.

--- dataLoaders.py
+++ dataLoaders.py
@@ -1,39 +1,30 @@
 import numpy as np
 
 
-def get_data_dimensions(data_file_name):
-    return data_file_name.split("-")[2].split("_")[0].split("x")[1]
+def get_data_dimensions(data_file):
+    return int(data_file.split("-")[2].split("_")[0].split("x")[1])
 
 
-def get_data_descriptors(data):
+def get_data_descriptors(data_file):
+    data = np.fromfile(data_file, np.float32)
+    descriptor_dim = get_data_dimensions(data_file)
+
     descriptors = []
-    descriptor_dim = get_data_dimensions(data)
     for i in range(0, len(data), descriptor_dim):
         descriptors.append(data[i:i + descriptor_dim])
-    return descriptors
+    return np.array(descriptors, dtype=np.float32)
 
 class Dataset():
 
-    def __init__(self, q_file, r_file):
-        self._q_data = np.fromfile(q_file, np.float32)
-        self._r_data = np.fromfile(r_file, np.float32)
+    def __init__(self, data_file):
+        self._data_descriptors = get_data_descriptors(data_file)
 
-        self._q_descriptors = get_data_descriptors(q_file)
-        self._r_descriptors = get_data_descriptors(r_file)
+    def _get_descriptors(self):
+        return self._data_descriptors
 
-    def _get_q(self):
-        return self._q_descriptors
+    def _set_data(self, new_data_file):
+        self._data_descriptors = get_data_descriptors(new_data_file)
 
-    def _get_r(self):
-        return self._r_descriptors
-
-    def _set_q(self, new_q_file):
-        self._q_descriptors = get_data_descriptors(new_q_file)
-
-    def _set_r(self, new_r_file):
-        self._r_descriptors = get_data_descriptors(new_r_file)
-
-    R = property(_get_r, _set_r)
-    Q = property(_get_q, _set_q)
+    descriptors = property(_get_descriptors, _set_data)
 
 
